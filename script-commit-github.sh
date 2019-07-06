05fb47d5d0ac20a115c92aba82a618947ac1b36e
cexposit <cexposit@ull.edu.es>
2019-07-06 23:03:49 +0100

    minor change in index

---
+++ script-commit-github.sh
@@ -0,0 +1,11 @@
+commitMessage=$1
+
+git rm --cached -r .
+git remote rm origin
+git init
+git add *
+git config --global user.email "cexposit@ull.edu.es"
+git config --global user.name "cexposit"
+git commit -m "${commitMessage}"
+git remote add origin git@github.com:KaizTen/kaizten.github.io
+git push -u origin master
