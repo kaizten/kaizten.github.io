9fc7e9d92700f070e80b1e8d61c7c3b7430639a2
cexposit <cexposit@ull.edu.es>
2021-01-02 23:34:39 +0000

    minor changes

--- script-commit-github.sh
+++ script-commit-github.sh
@@ -1,14 +1,25 @@
-read -p "# Enter your message in the commit: " message
+#!/bin/bash
+# Purpose: 
+# Author: Kaizten Analytics S.L. (info@kaizten.com)
+# License: GPL version 2.0 or above
+# -------------------------------------------------------------------------------------------------
 
-git add .
-git commit -m "${message}"
+main () {
+  read -p "# Enter your message in the commit: " message
+  git add .
+  set +e
+  git status | grep modified
+  if [ $? -eq 0 ]
+  then
+    set -e
+    git commit -m "${message}"
+    echo "# Pushing data to remote GitHub repository:"
+    git push
+  echo "# Done!"
+  else
+    set -e
+    echo "# Nothing to update"
+  fi
+}
 
-#if [ -n "$(git status - porcelain)" ];
-#then
-#	echo "IT IS CLEAN"
-#else
-	git status
-	echo "Pushing data to remote GitHub repository:"
-	git push -u origin master
-	echo "Done!"
-#fi
+main "$@"; exit
