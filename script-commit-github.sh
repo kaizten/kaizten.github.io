46b7f6a29f89ffbb61625c69bd67dfab65d6566c
cexposit <cexposit@ull.edu.es>
2021-03-11 18:33:17 +0000

    waste-recycling.json

--- script-commit-github.sh
+++ script-commit-github.sh
@@ -5,18 +5,25 @@
 # -------------------------------------------------------------------------------------------------
 
 main () {
-  git add .
-  set +e
-  if [ -n "$(git status --porcelain)" ]; then
-    read -p "# Enter your message in the commit: " message
-    set -e
-    git commit -m "${message}"
-    echo "# Pushing data to remote GitHub repository:"
-    git push
-    echo "# Done!"
+  echo "# Pulling repository:"
+  git pull
+  if [ $? -eq 0 ]; then
+    git add .
+    set +e
+    if [ -n "$(git status --porcelain)" ]; then
+      read -p "# Enter your message in the commit: " message
+      set -e
+      git commit -m "${message}"
+      echo "# Pushing data to remote GitHub repository:"
+      git push
+      echo "# Done!"
+    else
+      set -e
+      echo "# Nothing to update"
+    fi
   else
-    set -e
-    echo "# Nothing to update"
+    echo "# ERROR. Conflicts must be solved."
+    exit
   fi
 }
 
